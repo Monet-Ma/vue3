adfdea90a37947546397856f42c8e7d661179bf6
Monet-Ma <65269594+Monet-Ma@users.noreply.github.com>
2023-03-30 17:15:50 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,176 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div class="bgimg"></div>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  
+</template>
+
+<script>
+// import {onMounted,ref,computed} from "vue";
+// let account_item = ref([]);
+
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  //margin-top: 60px;
+}
+
+$ruler: 16px;
+$color-red: #AE1100;
+$color-bg: #EBECF0;
+$color-shadow: #BABECC;
+$color-white: #FFF;
+
+html {
+  // background:url('http://thekitemap.com/images/feedback-img.jpg') no-repeat;
+  // background-size: cover;
+  // height:100%;
+}
+.bgimg{
+  position: fixed;
+  width:100%;
+  height: 100%;
+  background:url('./assets/yuan2.webp') no-repeat;
+  background-size: cover;
+  height:100%;
+  background-position:center;
+  filter: blur(10px);
+  z-index: -1;
+}
+body{
+  margin: 0;
+}
+body,
+p,
+input,
+select,
+textarea,
+button {
+  font-family: 'Montserrat', sans-serif;
+  letter-spacing: -0.2px;
+  font-size: $ruler;
+}
+
+div,
+p {
+  color: $color-shadow;
+  text-shadow: 1px 1px 1px rgba(255,255,255,0.15);
+}
+
+
+form {
+  padding: $ruler;
+  width: $ruler*20;
+  margin: 0 auto;
+}
+
+.segment {
+  padding: $ruler*2 0;
+  text-align: center;
+}
+
+button,
+input {
+  border: 0;
+  outline: 0;
+  font-size: $ruler;
+  border-radius: $ruler*20;
+  padding: $ruler;
+  background-color: rgba(255,255,255,0.15);
+  text-shadow: 1px 1px 0 rgba(255,255,255,0.15);
+}
+
+label {
+  display: block;
+  margin-bottom: $ruler*1.5;
+  width: 100%;
+}
+
+input {
+  margin-right: $ruler/2;
+  box-shadow: inset 2px 2px 5px $color-shadow, inset -5px -5px 10px rgba(255,255,255,0.15);
+  width: 100%;
+  box-sizing: border-box;
+  transition: all 0.2s ease-in-out;
+  appearance: none;
+  -webkit-appearance: none;
+
+  &:focus {
+    box-shadow: inset 1px 1px 2px $color-shadow, inset -1px -1px 2px rgba(255,255,255,0.15);
+  }
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+
+button {
+  color: #61677C;
+  font-weight: bold;
+  box-shadow: -5px -5px 20px rgba(255,255,255,0.5), 5px 5px 20px rgba(255,255,255,0.5);
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+
+  &:hover {
+    box-shadow: -2px -2px 5px rgba(255,255,255,0.15), 2px 2px 5px rgba(255,255,255,0.15);
+  }
+
+  &:active {
+    box-shadow: inset 1px 1px 2px rgba(255,255,255,0.15), inset -1px -1px 2px rgba(255,255,255,0.15);
+  }
+
+  .icon {
+    margin-right: $ruler/2;
+  }
+
+  &.unit {
+    border-radius: $ruler/2;
+    line-height: 0;
+    width: $ruler*3;
+    height: $ruler*3;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 $ruler/2;
+    font-size: $ruler*1.2;
+
+    .icon {
+      margin-right: 0;
+    }
+  }
+
+  &.red {
+    display: block;
+    width: 100%;
+    color: $color-red;
+  }
+}
+
+.input-group {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  label {
+    margin: 0;
+    flex: 1;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="hello"
+       ref="editFormRef"
+       :model="account_form">
+    <div class="bl">
+      <p>虚妄时装</p>
+      <input class="inp" type="number" v-model="ssp_dress" />
+    </div>
+    <div class="bl">
+      <p>稀世时装</p>
+      <input class="inp" type="number" v-model="sp_dress" />
+    </div>
+    <div class="bl">
+      <p>奇珍时装</p>
+      <input class="inp" type="number" v-model="dress" />
+    </div>
+    <div class="bl">
+      <p>角色数</p>
+      <input class="inp" type="number" v-model="roles" />
+    </div>
+    <div class="bl">
+      <p>随从数量</p>
+      <input class="inp" type="number" v-model="retinue" />
+    </div>
+    <div class="bl">
+      <p>虚妄随身物品</p>
+      <input class="inp" type="number" v-model="ssp_goods" />
+    </div>
+    <div class="bl">
+      <p>稀世随身物品</p>
+      <input class="inp" type="number" v-model="sp_goods" />
+    </div>
+    <div class="bl">
+      <p>奇珍随身物品</p>
+      <input class="inp" type="number" v-model="goods" />
+    </div>
+    <div class="bl">
+      <p>头像</p>
+      <input class="inp" type="number" v-model="head" />
+    </div>
+    <div class="bl">
+      <p>头像框</p>
+      <input class="inp" type="number" v-model="head_frame" />
+    </div>
+    <div class="bl">
+      <p>涂鸦</p>
+      <input class="inp" type="number" v-model="graffiti" />
+    </div>
+    <div class="bl">
+      <p>等待动作</p>
+      <input class="inp" type="number" v-model="waiting_action" />
+    </div>
+    <div class="bl">
+      <p>个性动作</p>
+      <input class="inp" type="number" v-model="pursue_action" />
+    </div>
+    <div class="bl">
+      <p>追击音乐</p>
+      <input class="inp" type="number" v-model="music" />
+    </div>
+    <div class="bl">
+      <p>归宿分</p>
+      <input class="inp" type="number" v-model="score" />
+    </div>
+    <div class="bl">
+      <p>平台</p>
+      <input class="inp" type="text" v-model="platform" />
+    </div>
+    <div class="bl">
+      <p>上架售价</p>
+      <input class="inp" type="number" v-model="price" />
+    </div>
+    <div class="bl blbtn">
+      <button class="btn" @click="clk"> Predict</button>
+    </div>
+  </div>
+</template>
+
+<script>
+//
+//import {reactive} from "vue";
+import axios from "axios";
+
+// const account_form = reactive({
+//   ssp_dress: "",
+//   sp_dress: "",
+//   dress: "",
+//   roles: "",
+//   retinue: "",
+//   ssp_goods: "",
+//   sp_goods: "",
+//   goods: "",
+//   head: "",
+//   head_frame: "",
+//   graffiti: "",
+//   waiting_action: "",
+//   pursue_action: "",
+//   music: "",
+//   score: "",
+//   platform:"",
+//   price:"",
+// })
+const submitForm = () => {
+  axios.post('http://localhost:5000', 'hello').then(() => {
+
+  })
+}
+// const item_form = () => {
+//     axios.get("http://127.0.0.1:5000/",).then(res => {
+//       console.log(res.data)
+//   })
+// }
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  methods:{
+    clk(){
+      // alert("账号预估价值为：19697.91674")
+      submitForm()
+      // item_form()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content:space-between;
+  padding: 0 200px 0 200px;
+}
+.bl{
+  display: flex;
+  margin: 10px 70px 0px 70px;
+  width :500px;
+  justify-content:space-between;
+  align-items: center;
+  p{
+    font-family: STXingkai;
+    font-size: 32px;
+    color: #efefef;
+    width:220px;
+    margin:15px;
+  }
+  .btn{
+    background: rgba(255,255,255,0.15);
+  }
+}
+.blbtn{
+  justify-content: center;
+}
+.inp {
+  font-family: 'Montserrat', sans-serif;
+  letter-spacing: -0.2px;
+  width:200px;
+  height:50px;
+  background: rgba(255,255,255,0.15);
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+
+</style>
